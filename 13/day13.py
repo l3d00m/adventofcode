161c5b2e1b85d5226096825de78467c3ac0c6bc1
Thomas <substantialimpulse@pm.me>
2020-12-15 19:43:56 +0100

    day 13 improvements, still not there

--- 13/day13.py
+++ 13/day13.py
@@ -1,12 +1,11 @@
+import math
 from pathlib import Path
 from aocd import get_data
 lines = get_data(day=13, year=2020).splitlines()
 
 p = Path(__file__).resolve()
 with open(p.parent / 'in.txt') as f:
-    lines = f.read().splitlines()
-
-# lines = [line.replace("L","#") for line in lines]
+    lines2 = f.read().splitlines()
 
 current_time = int(lines[0])
 buses = [int(bus) for bus in lines[1].split(",") if bus != "x"]
@@ -20,35 +19,34 @@ for bus in buses:
     next_bus[bus] = bus_stop - current_time
 
 best_bus = min(next_bus, key=next_bus.get)
-print(best_bus*next_bus[best_bus])
+print(best_bus * next_bus[best_bus])
 
 
 # Part B
 buses = []
-elems = []
 for i, bus in enumerate(lines[1].split(",")):
     if bus == "x":
         continue
-    buses.append(int(bus))
-    elems.append(i)
+    buses.append((i, int(bus)))
+
+buses = sorted(buses, key=lambda x: x[1], reverse=True)
+max_offset, max_bus = buses[0]
+# Remove first element as we store it seperately
+buses.pop(0)
+min_offset, _ = buses[-1]
 
 
-def iterate_timestamp(expected_time):
-    for i, bus in enumerate(buses):
-        if (expected_time + elems[i]) % bus == 0:
-            if i+1 == len(elems):
-                return True
-        else:
-            return False
-
-
-max_index = buses.index(max(buses))
-# i = math.floor(100000000000000/buses[max_index])
-i = 0
+i = math.floor(100000000000000 / max_bus)
+#i = 0
 result = False
 while result is False:
     i += 1
-    expected_time = i * buses[max_index] - elems[max_index]
-    result = iterate_timestamp(expected_time)
+    expected_time = i * max_bus - max_offset
+    for offset, bus in buses:
+        if (expected_time + offset) % bus != 0:
+            break
+        elif offset == min_offset:
+            result = True
+            break
 
 print(expected_time)
--- 15/day15.py
+++ 15/day15.py
@@ -9,7 +9,7 @@ def get_number_a(start_turns, index_spoken):
         last_i = i - 1
         last_num = turns[last_i]
         last_spoken_index = last_i
-        for i2 in range(i-2, -1, -1):
+        for i2 in range(i - 2, -1, -1):
             if turns[i2] == last_num:
                 last_spoken_index = i2
                 break
@@ -30,8 +30,7 @@ def get_number_b(start_turns, index_spoken):
         # Store the number from this run, which is calculated in the last run
         this_number = next_age
         # Lookup correct age for next run before storing the current one
-        last_spoken_index = numbers.get(this_number, i)
-        next_age = i - last_spoken_index
+        next_age = i - numbers.get(this_number, i)
         # Now store the age for this turn
         numbers[this_number] = i
     return this_number
