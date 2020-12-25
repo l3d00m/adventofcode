17d2e0fd6aa50101a939eafa900cec1640367ba7
l3d00m <substantialimpulse@protonmail.com>
2020-12-26 00:15:49 +0100

    day 23 (and therefore AOC 2020) finally solved with linked lists!

--- 23/day23.py
+++ 23/day23.py
@@ -1,75 +1,69 @@
 from aocd import get_data
 inp = get_data(day=23, year=2020)
 
-inp = "389125467"
-in_cups = [int(char) for char in inp]
-cups = in_cups.copy()
+#inp = "389125467"
+cups = [int(char) for char in inp]
 
 
-def find_dest(current, sublist):
-    while current > 0:
-        try:
-            return sublist.index(current) + 5
-        except ValueError:
-            current -= 1
-    return sublist.index(max(sublist)) + 5
+def get_next(current, linked, num=3):
+    els = []
+    for i in range(num):
+        current = linked[current]
+        els.append(current)
+    return els, linked[current]
 
 
-def move(cups):
-    cups = list(cups)
-    dest = find_dest(cups[0], cups[4:])
-    cups[dest:dest] = cups[1:4]
-    del cups[1:4]
-    return collections.deque(cups)
+def get_current_list_str(linked, start=1):
+    val = linked[start]
+    res = str(start)
+    while val != start:
+        res += str(val)
+        val = linked[val]
+    return res
 
 
-import collections
-cups = collections.deque(cups)
-for _ in range(100):
-    cups = move(cups)
-    cups.rotate(-1)
+def move_with_linked(linked, current):
+    move_els, after_move_el = get_next(current, linked)
+    dest = current
+    while dest in move_els or dest == current:
+        dest -= 1
+        if dest < 1:
+            dest = len_cups
+    linked[current] = after_move_el
+    linked[move_els[2]] = linked[dest]
+    linked[dest] = move_els[0]
+    return linked
 
-while cups[0] != 1:
-    cups.rotate(1)
-cups.popleft()
-print(''.join([str(cup) for cup in cups]))
 
+def run(linked, iterations):
+    active_i = cups[0]
+    for i in range(iterations):
+        linked = move_with_linked(linked, active_i)
+        active_i = linked[active_i]
+    return linked
+
+
+linked_base = {}
+for i, el in enumerate(cups):
+    if i == len(cups) - 1:
+        break
+    linked_base[el] = cups[i + 1]
+
+## PART A
+linked = linked_base.copy()
+len_cups = max(cups)
+linked[el] = cups[0]
+
+linked = run(linked, 100)
+print(get_current_list_str(linked)[1:])
 
 ## PART B
+linked = linked_base.copy()
+len_cups = int(1e6)
+linked[el] = max(cups) + 1
+for i in range(max(cups) + 1, len_cups):
+    linked[i] = i + 1
+linked[len_cups] = cups[0]
 
-def move_with_deque(cups):
-    """Still much too slow, bruteforcing is probably not the solution"""
-    move_els = []
-    current = cups.popleft()
-    for i in range(3):
-        move_els.append(cups.popleft())
-    to_find = current
-    dest = None
-    while dest is None:
-        try:
-            dest = cups.index(to_find) + 1
-        except ValueError:
-            to_find -= 1
-            if to_find < 0:
-                dest = cups.index(max(cups)) + 1
-                break
-    for el in reversed(move_els):
-        cups.insert(dest, el)
-    cups.insert(0, current)
-    return cups
-
-
-import numpy as np
-cups = in_cups.copy()
-cups += np.arange(max(cups) + 1, 1e6 + 1, dtype=int).tolist()
-
-#cups = in_cups + np.arange(max(in_cups) + 1, 25 + 1, dtype=int).tolist()
-#cups.rotate(-len(in_cups))
-
-cups = collections.deque(cups)
-for i in range(int(10e6)):
-    #cups.rotate(i*4)
-    #print(f"{i:02d}: " + ' | '.join([f"{cup:02d}" for cup in cups]))
-    #cups.rotate(-i*4)
-    cups = move_with_deque(cups)
-    cups.rotate(-1)
+linked = run(linked, int(10e6))
+print(linked[1] * linked[linked[1]])
