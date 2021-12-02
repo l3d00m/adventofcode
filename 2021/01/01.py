d164a7e46e6563d2e8656bbbb1352df762591651
Thomas Lübbehüsen <tluebbehuesen@de.adit-jv.com>
2021-12-02 18:33:27 +0100

    day 1 solution; day2 prep

--- 2021/01/01.py
+++ 2021/01/01.py
@@ -1,4 +1,35 @@
+import numpy as np
 from aocd import get_data
-inp = get_data(day=1, year=2021)
 
-print(inp)
+inp = get_data(day=1, year=2021)
+lines = inp.splitlines()
+numbers = [int(line) for line in lines]
+arrayinp = np.asarray(numbers)
+
+#print(numbers)
+
+
+## Part A
+def count_val_increase(numbers):
+    prev_item = numbers[0]
+    count = 0
+    for item in numbers:
+        if item > prev_item:
+            count += 1
+        prev_item = item
+    return count
+
+
+print(count_val_increase(numbers))
+
+## Part A with np
+shiftedarray = np.roll(arrayinp, 1)
+shiftedarray[0] = arrayinp[1]
+shiftedarray[-1] = arrayinp[-1]
+
+print((arrayinp > shiftedarray).sum())
+
+
+## Part B
+averages = np.convolve(numbers, np.ones(3, dtype=int), 'valid')
+print(count_val_increase(averages))
---
+++ 2021/02/02.py
@@ -0,0 +1,11 @@
+import numpy as np
+from aocd import get_data
+
+inp = get_data(day=2, year=2021)
+lines = inp.splitlines()
+numbers = [int(line) for line in lines]
+arrayinp = np.asarray(numbers)
+
+#print(numbers)
+
+## Part A
