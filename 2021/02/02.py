2f05f1a7a2818c81f128ef4a12e674f59f2b0d19
Thomas <substantialimpulse@pm.me>
2021-12-02 19:43:09 +0100

    day2 solved & day 3 prep

--- 2021/02/02.py
+++ 2021/02/02.py
@@ -8,7 +8,7 @@ lines = inp.splitlines()
 
 #print(numbers)
 
-## Part A
+print("# Part A")
 depth = 0
 horizontal = 0
 for item in lines:
@@ -19,6 +19,28 @@ for item in lines:
             depth += int(num)
         case ['up', num]:
             depth -= int(num)
+        case _:
+            raise Exception("not known")
+
+
+print(f"Depth {depth}")
+print(f"horizontal {horizontal}")
+print(f"Result {depth*horizontal}")
+
+
+print("# Part B")
+depth = 0
+horizontal = 0
+aim = 0
+for item in lines:
+    match item.split():
+        case ['forward', num]:
+            horizontal += int(num)
+            depth += aim * int(num)
+        case ['down', num]:
+            aim += int(num)
+        case ['up', num]:
+            aim -= int(num)
         case _:
             raise Exception("not known")
 
--- 2021/03/03.py
+++ 2021/03/03.py
@@ -1,7 +1,7 @@
 import numpy as np
 from aocd import get_data
 
-inp = get_data(day=0, year=2021)
+inp = get_data(day=3, year=2021)
 lines = inp.splitlines()
 #numbers = [int(line) for line in lines]
 #arrayinp = np.asarray(numbers)
