75cdd603f9dd6af692726373647914b5934a7a0f
Thomas <substantialimpulse@pm.me>
2021-12-02 19:21:18 +0100

    day 2 part 1 with structural pattern matching, requires python>=3.10

--- 2021/02/02.py
+++ 2021/02/02.py
@@ -3,9 +3,25 @@ from aocd import get_data
 
 inp = get_data(day=2, year=2021)
 lines = inp.splitlines()
-numbers = [int(line) for line in lines]
-arrayinp = np.asarray(numbers)
+#numbers = [int(line) for line in lines]
+#arrayinp = np.asarray(numbers)
 
 #print(numbers)
 
 ## Part A
+depth = 0
+horizontal = 0
+for item in lines:
+    match item.split():
+        case ['forward', num]:
+            horizontal += int(num)
+        case ['down', num]:
+            depth += int(num)
+        case ['up', num]:
+            depth -= int(num)
+        case _:
+            raise Exception("not known")
+
+print(f"Depth {depth}")
+print(f"horizontal {horizontal}")
+print(f"Result {depth*horizontal}")
