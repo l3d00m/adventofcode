1b7c891959ab2f2d3e3a7796bb0915578136738d
Thomas <substantialimpulse@pm.me>
2021-12-09 20:56:13 +0100

    day10 prep

---
+++ 2021/10/10.py
@@ -0,0 +1,17 @@
+import numpy as np
+from aocd import get_data
+
+inp = get_data(day=10, year=2021)
+lines = inp.splitlines()
+#numbers = [int(line) for line in lines]
+#arrayinp = np.asarray(numbers)
+
+#print(numbers)
+
+print("---Part A---")
+
+
+print("---Part B---")
+
+
+print("end")
